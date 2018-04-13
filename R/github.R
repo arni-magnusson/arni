959ce4045dea638ba8a3647ae2b8ff2583db3bfe
Arni Magnusson <arnima@hafro.is>
2018-04-13 11:41:00 +0200

    New function github()

---
+++ R/github.R
@@ -0,0 +1,19 @@
+#' GitHub User
+#'
+#' Look up email address of GitHub user.
+#'
+#' @param user GitHub username.
+#'
+#' @return
+#' Vector of strings containing \code{@} sign.
+#'
+#' @export
+
+github <- function(user)
+{
+  url <- paste0("https://api.github.com/users/", user, "/events/public")
+  x <- suppressWarnings(readLines(url))
+  x <- unlist(strsplit(x, "\""))
+  x <- grep("@", x, value=TRUE)
+  x
+}
