abe7ca6331a4e4a71c35d644e7af728328da5906
Arni Magnusson <arni.magnusson@ices.dk>
2020-06-05 19:50:39 +0200

    Support xlim and ylim

--- R/plot.loess.R
+++ R/plot.loess.R
@@ -5,6 +5,8 @@
 #' @param x a fitted loess model.
 #' @param lwd the line width.
 #' @param line.col the line colour.
+#' @param xlim passed to \code{plot}.
+#' @param ylim passed to \code{plot}.
 #' @param xlab passed to \code{plot}.
 #' @param ylab passed to \code{plot}.
 #' @param \dots passed to \code{plot}.
@@ -15,20 +17,25 @@
 #'
 #' @export
 
-plot.loess <- function(x, lwd=2, line.col="black", xlab=NULL, ylab=NULL, ...)
+plot.loess <- function(x, lwd=2, line.col="black", xlim=NULL, ylim=NULL,
+                       xlab=NULL, ylab=NULL, ...)
 {
   model <- x
   x   <- model$x
   y   <- model$y
   fit <- model$fit
 
+  if(is.null(xlim))
+    xlim <- range(x)
+  if(is.null(ylim))
+    ylim <- c(min(y,fit), max(y,fit))
+
   if(is.null(xlab))
     xlab <- as.character(terms(model))[3]
   if(is.null(ylab))
     ylab <- as.character(terms(model))[2]
 
-  plot(x, y, xlim=range(x), ylim=c(min(y,fit),max(y,fit)), xlab=xlab, ylab=ylab,
-       ...)
+  plot(x, y, xlim=xlim, ylim=ylim, xlab=xlab, ylab=ylab, ...)
   lines(x[order(x)], fit[order(x)], lwd=lwd, col=line.col)
 
   invisible(NULL)
