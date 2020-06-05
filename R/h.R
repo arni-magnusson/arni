7a57ea254ae30c0dedb82c666acde70efb53fc98
Arni Magnusson <arni.magnusson@ices.dk>
2020-06-05 20:07:46 +0200

    Support h() in Emacs ESS

--- R/h.R
+++ R/h.R
@@ -6,11 +6,15 @@
 #'        log).
 #' @param reverse whether to show lines in reverse order (not useful when there
 #'        are continuation lines).
-#' @param \dots passed to \code{history}.
+#' @param \dots passed to \code{history} and \code{file.show}.
 #'
-#' @return Shows command history in external editor.
+#' @note
+#' Shorthand notation for \code{history()} with improved support for Emacs ESS.
 #'
-#' @note Shorthand notation for a common task.
+#' The base \code{history} function returns an error inside an Emacs session. As
+#' a workaround, this function checks if it is run from inside Emacs and if that
+#' is the case, it calls \code{file.show} to show the \file{.Rhistory} file
+#' (ignoring both \code{max.show} and \code{reverse}).
 #'
 #' @importFrom utils history
 #'
@@ -18,5 +22,13 @@
 
 h <- function(max.show=Inf, reverse=TRUE, ...)
 {
-  history(max.show=max.show, reverse=reverse, ...)
+  if(Sys.getenv("INSIDE_EMACS") == "")
+  {
+    history(max.show=max.show, reverse=reverse, ...)
+  }
+  else
+  {
+    r.histfile <- Sys.getenv("R_HISTFILE")
+    file.show(if(r.histfile == "") ".Rhistory" else r.histfile, ...)
+  }
 }
