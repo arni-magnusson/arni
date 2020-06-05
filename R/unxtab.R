049396de74fcf2179ac32ab912743b82d6fc6e56
Arni Magnusson <arni.magnusson@ices.dk>
2020-06-05 19:52:39 +0200

    Simplify documentation and implementation of unxtab()

--- R/unxtab.R
+++ R/unxtab.R
@@ -1,32 +1,32 @@
 #' Uncrosstab
 #'
-#' Coerce crosstab table into three column data frame.
+#' Convert a table from crosstab format to long format.
 #'
-#' @param x a crosstab table (data frame, matrix, or table), with important row
+#' @param x a crosstab table (data frame, matrix, or table) with descriptive row
 #'        names and column names.
 #' @param labels a vector of three strings, used as column names for the
 #'        resulting data frame.
 #'
-#' @return Data frame (3 cols) that can be crosstabbed to look like \code{x}.
+#' @return
+#' Data frame in long format (3 columns) that can be crosstabbed to look like
+#' \code{x}.
 #'
 #' @note
-#' Use dimnames like \code{"1990"} instead of \code{"Year1990"} for numeric
-#' entries in resulting data frame.
+#' Use dimnames like \code{"1990"} instead of \code{"Year1990"} to end up with
+#' numeric entries in resulting data frame.
 #'
 #' Another approach is \code{as.data.frame(as.table(as.matrix(x)))}.
 #'
 #' @export
 
-unxtab <- function(x, labels=c(rows="Year",cols="Age",vals="Value"))
+unxtab <- function(x, labels=c("Year","Age","Value"))
 {
-  r.count <- nrow(x)
-  c.count <- ncol(x)
   r.names <- simplify(rownames(x))
   c.names <- simplify(colnames(x))
 
-  col.1 <- rep(r.names, each=c.count)
-  col.2 <- rep(c.names, times=r.count)
-  col.3 <- as.vector(t(as.matrix(x)))
+  col.1 <- rep(r.names, each=ncol(x))
+  col.2 <- rep(c.names, times=nrow(x))
+  col.3 <- c(t(as.matrix(x)))
 
   output <- data.frame(col.1, col.2, col.3)
   names(output) <- labels
