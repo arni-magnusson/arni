2bf01cb6d46f773450250bc0c75f7369ba7d6eb0
Arni Magnusson <thisisarni@gmail.com>
2021-02-02 01:21:56 +0100

    New function psl()

---
+++ R/psl.R
@@ -0,0 +1,17 @@
+#' Print Simple List
+#'
+#' Print simple list.
+#'
+#' @param \dots passed to \code{print.simple.list}.
+#'
+#' @note Shorthand notation for \code{print.simple.list()}.
+#'
+#' @examples
+#' pls(options())
+#'
+#' @export
+
+pls <- function(...)
+{
+  print.simple.list(...)
+}
