20739e6b2c6a7637fcb21a2a4a148f25938a5d40
Arni Magnusson <thisisarni@gmail.com>
2022-03-04 04:48:19 +1100

    Replace character(0) with NULL

--- R/diff.character.R
+++ R/diff.character.R
@@ -9,20 +9,38 @@
 #' @return List containing lines that are different between the files.
 #'
 #' @examples
+#' \dontrun{
+#' diff("this.txt", "that.txt")
+#'
 #' x <- system.file("DESCRIPTION", package="base")
 #' y <- system.file("DESCRIPTION", package="datasets")
 #' diff(x, y)
+#' }
 #'
 #' @export
 #' @export diff.character
 
 diff.character <- function(x, y, ...)
 {
+  ## 1  Read files
   A <- readLines(x)
   B <- readLines(y)
+
+  ## 2  Compare
   diffA <- setdiff(A, B)
   diffB <- setdiff(B, A)
   out <- list(diffA, diffB)
   names(out) <- c(x, y)
-  out
+
+  ## 3  Replace character(0) with NULL
+  out[sapply(out, length) == 0] <- NULL
+  if(length(out) == 0)
+  {
+    out <- NULL
+    invisible(out)
+  }
+  else
+  {
+    out
+  }
 }
